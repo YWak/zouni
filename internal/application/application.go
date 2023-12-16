3ec60befcb48ff6e904e9975ba51c761c151fa0c
YWak <ywak1984@gmail.com>
2023-12-17 06:10:22 +0900

    add file parsing

--- internal/application/application.go
+++ internal/application/application.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"go/ast"
 	"go/build"
+	"go/parser"
 	"go/token"
+	"os"
 	"path/filepath"
 
 	"github.com/ywak/zouni/internal/config"
@@ -14,6 +16,7 @@ type Application struct {
 	fset   *token.FileSet
 	root   *build.Package
 	pkgs   map[string]*build.Package
+	files  map[string]*ast.File
 	tokens map[ast.Decl]bool
 	config *config.ZouniConfig
 }
@@ -25,6 +28,7 @@ func Load(cfg *config.ZouniConfig, rootDir string) (*Application, error) {
 	app := Application{
 		fset:   fset,
 		pkgs:   map[string]*build.Package{},
+		files:  map[string]*ast.File{},
 		tokens: map[ast.Decl]bool{},
 		config: cfg,
 	}
@@ -50,6 +54,33 @@ func Load(cfg *config.ZouniConfig, rootDir string) (*Application, error) {
 		bpkg := pkgQueue[0]
 		pkgQueue = pkgQueue[1:]
 
+		files := make([]string, 0, len(bpkg.GoFiles)+len(bpkg.CgoFiles))
+		files = append(files, bpkg.GoFiles...)
+		files = append(files, bpkg.CgoFiles...)
+
+		for _, f := range files {
+			var fname string
+			if bpkg.Name == "main" {
+				fname = f
+			} else {
+				fname = filepath.Join(bpkg.ImportPath, f)
+			}
+			path := filepath.Join(bpkg.Dir, f)
+			bytes, err := os.ReadFile(path)
+			if err != nil {
+				return nil, err
+			}
+			astFile, err := parser.ParseFile(fset, fname, bytes, parser.ParseComments)
+			if err != nil {
+				return nil, err
+			}
+			app.files[path] = astFile
+			cfg.Logger().Printf("parsed %s", path)
+			for i, v := range astFile.Decls {
+				cfg.Logger().Printf("decls[%d] = %v", i, v)
+			}
+		}
+
 		// 全importを探す
 		for _, importValue := range bpkg.Imports {
 			importDir, err := app.getImportPath(importValue, bpkg.Dir)
@@ -64,6 +95,7 @@ func Load(cfg *config.ZouniConfig, rootDir string) (*Application, error) {
 
 			cfg.Logger().Printf("next package = %v", nextPkg)
 			app.pkgs[importValue] = nextPkg
+			pkgQueue = append(pkgQueue, nextPkg)
 		}
 
 		cfg.Logger().Printf("%v", *bpkg)
